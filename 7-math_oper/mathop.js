45fed35afa4492500be703c1597ae8b5f892f35c
sfarooq80 <89254950+sfarooq80@users.noreply.github.com>
2023-08-15 16:00:34 +0500

    work

---
+++ 7-math_oper/mathop.js
@@ -0,0 +1,13 @@
+"use strict";
+let operand_1 = 2;
+let operand_2 = 4;
+console.log(operand_1 * operand_2); //multiply
+operand_1 = 16;
+operand_2 = 2;
+console.log(operand_1 / operand_2); //divide
+operand_1 = 4;
+operand_2 = 4;
+console.log(operand_1 + operand_2); //addition
+operand_1 = 12;
+operand_2 = 4;
+console.log(operand_1 - operand_2); //subtraction
