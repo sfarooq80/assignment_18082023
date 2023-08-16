20d47ac2182707ddf36de3915570014bb1ee361b
sfarooq80 <89254950+sfarooq80@users.noreply.github.com>
2023-08-16 12:57:46 +0500

    work

---
+++ 13-print_arr_advanced/print_arr_msgadv.js
@@ -0,0 +1,6 @@
+"use strict";
+// Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+let items = ["Honda", "Peshawar", "Imsciences"];
+console.log(`i would like to own a ${items[0]} motorcycle`);
+console.log(`i Live in ${items[1]} `);
+console.log(`i do job in  ${items[2]} `);
---
+++ 14-print_msg/print_msg.js
@@ -0,0 +1,6 @@
+"use strict";
+// Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+let items = ["Shaheer", "zuhaib", "shadab"];
+for (let i = 0; i < items.length; i++) {
+    console.log(`Hi ${items[i]} i would like to invite you for launch \n`);
+}
