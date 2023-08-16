8a9737add8cf6d6eba020f41cdf75588ef956760
sfarooq80 <89254950+sfarooq80@users.noreply.github.com>
2023-08-16 11:53:15 +0500

    work

---
+++ 12-print_arr_msg/print_arr_msg.js
@@ -0,0 +1,6 @@
+"use strict";
+// Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+let names = ["zuhaib", "shaheer", "waqas", "shadab"];
+for (let i = 0; i < names.length; i++) {
+    console.log(`Hi,${names[i]}`);
+}
