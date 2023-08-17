73d1ae72a4642a1f73437e336476a156f0eb0940
sfarooq80 <sfarooq80@gmail.com>
2023-08-17 19:53:36 +0500

    work

---
+++ 18-fiveplaces/fiveplaces.js
@@ -0,0 +1,8 @@
+"use strict";
+// Store the locations in a array. Make sure the array is not in alphabetical order.
+let locations = ["naraan", "kaghan", "hunza", "skardu", "kewai"];
+console.log(locations);
+let locations_sort = locations.sort();
+console.log(locations_sort);
+let locations_reverse = locations.reverse();
+console.log(locations_reverse);
