f8d56394360cc41ea0a5aa0f98682e12af922b34
sfarooq80 <sfarooq80@gmail.com>
2023-08-17 20:05:58 +0500

    work

---
+++ 19-dinner_guest/dinnerguest.js
@@ -0,0 +1,4 @@
+"use strict";
+// print a message indicating the number of people you are inviting to dinner.
+let inviting_for_dinner = ["Shaheer", "zuhaib", "shadab"];
+console.log(inviting_for_dinner.length);
---
+++ 19-dinner_guest/dinnerguest.ts
@@ -0,0 +1,5 @@
+// print a message indicating the number of people you are inviting to dinner.
+
+let inviting_for_dinner:string[] = ["Shaheer","zuhaib","shadab"];
+
+console.log(inviting_for_dinner.length)
