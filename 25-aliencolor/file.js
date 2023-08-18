ceaea642eea6c3a9d1f322fc3d700c43df25be9c
sfarooq80 <89254950+sfarooq80@users.noreply.github.com>
2023-08-18 12:51:21 +0500

    work

---
+++ 25-aliencolor/file.js
@@ -0,0 +1,9 @@
+"use strict";
+// They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+let alien_color = "green";
+if (alien_color == "green") {
+    console.log("player just earned 5 points.");
+}
+else {
+    console.log("");
+}
--- 25-aliencolor/file.ts
+++ 25-aliencolor/file.ts
@@ -1,10 +1,10 @@
 // They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
 
-let objects = {
-        name : "arsalan",
-        occupation:"webdeveloper",
-        education : "BS Computer Science",
-        Address : "Peshawar"
+let alien_color  = "green";
+
+if(alien_color=="green"){
+        console.log("player just earned 5 points.");
+}else{
+        console.log("");
 }
 
-console.log(objects.name)
