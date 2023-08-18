cbd2625e5ef81b1af87b999ec3c18c97aece4652
sfarooq80 <89254950+sfarooq80@users.noreply.github.com>
2023-08-18 11:21:35 +0500

    work

---
+++ 26-/file.ts
@@ -0,0 +1,10 @@
+// They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+let objects = {
+        name : "arsalan",
+        occupation:"webdeveloper",
+        education : "BS Computer Science",
+        Address : "Peshawar"
+}
+
+console.log(objects.name)
