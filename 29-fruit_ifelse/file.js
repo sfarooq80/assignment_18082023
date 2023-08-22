94338097b2a4fa22aa80bb16e8cc3eadde768509
sfarooq80 <89254950+sfarooq80@users.noreply.github.com>
2023-08-22 14:35:05 +0500

    work

---
+++ 29-fruit_ifelse/file.js
@@ -0,0 +1,23 @@
+"use strict";
+// Write five if statements. Each should check whether a certain kind of fruit is in your array. If the fruit is in your array, the if block should print a statement, such as You really like bananas!
+let fruits = ['apple', 'orange', 'mango', 'banana', 'strawbery'];
+for (let i = 0; i < fruits.length; i++) {
+    if (fruits[i] == "strawbery") {
+        console.log("you really like strawbery");
+    }
+}
+for (let i = 0; i < fruits.length; i++) {
+    if (fruits[i] == "apple") {
+        console.log("you really like apple");
+    }
+}
+for (let i = 0; i < fruits.length; i++) {
+    if (fruits[i] == "orange") {
+        console.log("you really like orange");
+    }
+}
+for (let i = 0; i < fruits.length; i++) {
+    if (fruits[i] == "mango") {
+        console.log("you really like mango");
+    }
+}
--- 31-/file.js
+++ 31-/file.js
@@ -1,11 +1,11 @@
+"use strict";
 // Hello Admin: Make a array of five or more usernames, including the name 'admin'. Imagine you are writing code that will print a greeting to each user after they log in to a website. Loop through the array, and print a greeting to each user:
-
-let users:string[] = ['admin','kashif','umer','talha','mehmood'];
-
-for(let i=0; i<users.length; i++){
-        if(users[i]=="admin"){
-                console.log(`welcome administrator with all previliages`)
-        }else{
+let users = ['admin', 'kashif', 'umer', 'talha', 'mehmood'];
+for (let i = 0; i < users.length; i++) {
+    if (users[i] == "admin") {
+        console.log(`welcome administrator with all previliages`);
+    }
+    else {
         console.log(`Hello ${users[i]}`);
-        }
+    }
 }
