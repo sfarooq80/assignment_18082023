85bb603fc8d25843a46b852d903d6f23ed4b4c2e
sfarooq80 <89254950+sfarooq80@users.noreply.github.com>
2023-08-22 14:53:10 +0500

    work

---
+++ 31-checkarray/file.js
@@ -0,0 +1,7 @@
+"use strict";
+// No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
+// • If the list is empty, print the message We need to find some users!
+let user_1 = [];
+if (user_1.length == 0) {
+    console.log("wemust have some users");
+}
---
+++ 31-checkarray/file.ts
@@ -0,0 +1,8 @@
+// No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
+// • If the list is empty, print the message We need to find some users!
+
+let user_1:any = [];
+
+if(user_1.length==0){
+    console.log("wemust have some users");
+}
